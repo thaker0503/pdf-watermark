4ee21b87f3f05b376e9052482518066e3b669841
Yatharth Thaker <52533890+thaker0503@users.noreply.github.com>
2022-11-03 12:47:12 +0530

    afaf

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var uploadRouter = require('./routes/upload');
+var waterMarkRouter = require('./routes/addwatermark');
 // var uploadPageRouter = require('./routes/uploadpage');
 
 var app = express();
@@ -24,7 +25,11 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/upload', uploadRouter);
+app.use('/addwatermark', waterMarkRouter);
 // app.use('/uploadpage', uploadPageRouter)
+
+
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
