11439bb2f0dc55205ae45f788a211faaa5743329
Yatharth Thaker <52533890+thaker0503@users.noreply.github.com>
2022-11-07 16:19:11 +0530

    Authentication Register

--- routes/registration.js
+++ routes/registration.js
@@ -2,20 +2,34 @@ var express = require("express");
 var router = express.Router();
 // const mysql = require('mysql');
 const bcrypt = require('bcrypt');
+const { getDatabase, ref, set } = require("firebase/database");
+
+// const db = getDatabase();
 // const e = require("express");
 
 
-function encrypt(password) {
+async function encrypt(password) {
+  // const hash = await bcrypt.hash(password, 10)
   return bcrypt.hash(password, 10)
 }
 router.post('/', async (req, res) => {
   
-  const hash = await bcrypt.hash(req.body.password, 10)
 
-  encrypt(req.body.password);
+  req.body.passweord = await encrypt(req.body.password);
+  await writeUserData(req.body.email, req.body.password);
+  
   res.send("Checked")
 
  
-})
+});
+
+async function writeUserData(email,password) {
+  const db = getDatabase();
+  await set(ref(db, 'userDetails/' + email), {
+    email,
+    password : password
+  });
+}
+
 
 module.exports = router;
