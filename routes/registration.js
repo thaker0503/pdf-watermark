8d36b252dcbb8ab1f76bb980dff3bb4c7c5ba962
AmishaRadadiya <108644942+AmishaRadadiya@users.noreply.github.com>
2022-11-07 15:39:36 +0530

    Update 2

--- routes/registration.js
+++ routes/registration.js
@@ -1,9 +1,22 @@
 var express = require("express");
 var router = express.Router();
 // const mysql = require('mysql');
+const firebase = require('firebase');
 const bcrypt = require('bcrypt');
 // const e = require("express");
 
+var firebaseConfig = {
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
+}
+
+firebase.initializeApp(firebaseConfig)
+let database = firebase.database();
 
 function encrypt(password) {
   return bcrypt.hash(password, 10)
@@ -13,9 +26,7 @@ router.post('/', async (req, res) => {
   const hash = await bcrypt.hash(req.body.password, 10)
 
   encrypt(req.body.password);
-  res.send("Checked")
-
- 
+  res.send("Checked") 
 })
 
 module.exports = router;
