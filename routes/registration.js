e0667c076be11518124e4f1538fe5cb96bf911e5
Yatharth Thaker <52533890+thaker0503@users.noreply.github.com>
2022-11-07 16:55:49 +0530

    Authentication complete

--- routes/registration.js
+++ routes/registration.js
@@ -1,28 +1,19 @@
 var express = require("express");
 var router = express.Router();
-// const mysql = require('mysql');
 const bcrypt = require('bcrypt');
 const { getDatabase, ref, set } = require("firebase/database");
 
-// const db = getDatabase();
-// const e = require("express");
-
 
 async function encrypt(password) {
-  // const hash = await bcrypt.hash(password, 10)
   return bcrypt.hash(password, 10)
 }
-router.post('/', async (req, res) => {
-  
 
-  req.body.passweord = await encrypt(req.body.password);
+router.post('/', async (req, res) => {
+  req.body.password = await encrypt(req.body.password);
   await writeUserData(req.body.email, req.body.password);
-  
   res.send({
     msg: "User Registered Successfully"
-  })
-
- 
+  });
 });
 
 async function writeUserData(email,password) {
