e5ff949e47f501e9fa1b547cff59c023026c79b8
Yatharth Thaker <52533890+thaker0503@users.noreply.github.com>
2022-11-08 18:06:04 +0530

    Authentication Update

--- routes/login.js
+++ routes/login.js
@@ -1,30 +1,33 @@
 const express = require('express');
 const router = express.Router()
 const bcrypt = require('bcrypt');
-const { getDatabase, ref, onValue, orderByChild, query, equalTo, child, DataSnapshot } = require("firebase/database");
+const { getDatabase, ref, child, get } = require("firebase/database");
 
 router.post('/', async (req, res) => {
-    
   const db = getDatabase();
-  const emailCheckRef = ref(db, 'userDetails');
-  // const teat = orderByChild(emailCheckRef);
-  onValue(ref(child(emailCheckRef, orderByChild('email'), equalTo('test2@test.com'))), (snapshot) => {
-    console.log(snapshot)
+  const dbRef = ref(db);
+  get(child(dbRef, 'userDetails')).then((snapshot) => {
+    let a = []
+    Object.values(snapshot.val()).forEach((i, k) => {
+      a.push({ ...i, ...{ uuid: Object.keys(snapshot.val())[k] } })
+    });
+    console.log(a)
+    // a.forEach(item => {
+    //   if (item.email === req.body.email && compare(req.body.password, item.password)) {
+    //     res.send({
+    //       msg: "User Logged In",
+    //       msgType: "Success"
+    //     })
+    //   } else if (item.email === req.body.email && !compare(req.body.password, item.password)) {
+    //     res.send({
+    //       msg: "Wrong Password",
+    //       msgType: "Error"
+    //     })
+    //   }
+    // })
   })
+})
 
-
-
-  // onValue(emailCheckRef, async (snapshot) => {
-  //   const data = snapshot.val();
-  //   // const check = await compare(req.body.password, data.password);
-  //   // console.log(check);
-  //   // check ? res.send({
-  //   //   msg:"Correct Password"
-  //   // }) : res.send({
-  //   //   msg:"Wrong Password"
-  //   // })
-  // });
-  })
 
 function compare(plaintextPassword, hash) {
   return bcrypt.compare(plaintextPassword, hash)
