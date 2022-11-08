4f4c7a80c0f505454df3ef501ef9b751aa21e93a
Yatharth Thaker <52533890+thaker0503@users.noreply.github.com>
2022-11-08 17:53:34 +0530

    Authentication Update

--- routes/registration.js
+++ routes/registration.js
@@ -1,7 +1,7 @@
 var express = require("express");
 var router = express.Router();
 const bcrypt = require('bcrypt');
-const { getDatabase, ref, set } = require("firebase/database");
+const { getDatabase, ref, set, get, child } = require("firebase/database");
 const { v4: uuid } = require('uuid');
 
 router.get('/', function (req, res, next) {
@@ -23,19 +23,43 @@ function encrypt(password) {
 router.post('/', async (req, res) => {
   // console.log(req.body)
   const encPassword = await encrypt(req.body.password);
-  await writeUserData(req.body.email, encPassword);
-  res.send({
-    msg: `User Registered Successfully with ${req.body.email}`,
-    msgType: "Success"
-  });
+  const response = await writeUserData(req.body.email, encPassword);
+  console.log(response);
+  res.send(response);
 });
 
 async function writeUserData(email,password) {
   const db = getDatabase();
-  await set(ref(db, 'userDetails/' + uuid()), {
-    email: email,
-    password : password
-  });
+  const dbRef = ref(db);
+  return new Promise((resolve, reject) => {
+    get(child(dbRef, 'userDetails')).then((snapshot) => {
+      let a = []
+      Object.values(snapshot.val()).forEach((i, k) => {
+        a.push({ ...i, ...{ uuid: Object.keys(snapshot.val())[k] } })
+      });
+      a.forEach(item => {
+        if (item.email === email) {
+          resolve({
+            msg: "User Already Exists",
+            msgType: "Error"
+          })
+        }
+        else {
+          set(ref(db, 'userDetails/' + uuid()), {
+            email: email,
+            password: password
+          }).then(resolve({
+            msg: "User registered Successfully",
+            msgType: "Success"
+          })).catch(err => {
+            if (err) {
+              reject(err)
+            }
+          })
+        }
+      })
+    })
+  })
   
 }
 
