ca83e8db2f317d85a470ceb9f60e16135dcb96db
Yatharth Thaker <52533890+thaker0503@users.noreply.github.com>
2022-11-09 10:18:46 +0530

    Authentication Update

--- routes/login.js
+++ routes/login.js
@@ -8,26 +8,43 @@ router.post('/', async (req, res) => {
   const dbRef = ref(db);
   get(child(dbRef, 'userDetails')).then((snapshot) => {
     if (snapshot.val() !== null) {
-      let a = []
+      let a = [], email = [];
       Object.values(snapshot.val()).forEach((i, k) => {
         a.push({ ...i, ...{ uuid: Object.keys(snapshot.val())[k] } })
       });
       // console.log(a)
-      a.forEach(async (item) => {
-        const password = await compare(req.body.password, item.password);
-        // console.log(password)
-        if (item.email === req.body.email && password) {
-          res.send({
-            msg: "User Logged In",
-            msgType: "Success"
-          })
-        } else if (item.email === req.body.email && !password) {
-          res.send({
-            msg: "Wrong Password",
+      a.forEach(item => {
+        console.log("Email ==========>", item.email)
+        email.push(item.email)
+      })
+      if (email.includes(req.body.email)) {
+        a.forEach(async (item) => {
+          const password = await compare(req.body.password, item.password);
+          // console.log(password)
+          if (item.email === req.body.email && password) {
+            res.send({
+              msg: "User Logged In",
+              msgType: "Success"
+            })
+          } else if (item.email === req.body.email && !password) {
+            res.send({
+              msg: "Wrong Password",
+              msgType: "Error"
+            })
+          }
+        })
+        
+      } else {
+        res.send({
+          msg: "User Not Regtistered",
+          msgType: "Error"
+        })
+      }
+    } else {
+      res.send({
+            msg: "User Not Registered",
             msgType: "Error"
           })
-        }
-      })
     }
   })
 })
