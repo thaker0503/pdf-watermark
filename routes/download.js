51a3a1765a5ef71a78cdcaf50d682a90354582d2
Yatharth Thaker <52533890+thaker0503@users.noreply.github.com>
2022-11-10 15:50:09 +0530

    Updates in pdf download

--- routes/download.js
+++ routes/download.js
@@ -3,7 +3,7 @@ var router = express.Router();
 const request = require('request');
 
 router.post('/', (req, res) => {
-    // res.setHeader('content-disposition', 'attachment; filename=quote.pdf');
+    res.setHeader('content-disposition', 'attachment; filename=quote.pdf');
     // request('https://firebasestorage.googleapis.com/v0/b/pdf-watermark-a2d77.appspot.com/o/5iQ%20Not%20this%20time%20_20220929_0001-updated-1668065421001?alt=media&token=729df798-1adc-47f0-8d5a-160f24c7b73d').pipe(res)
     console.log(req.body.link)
     request(req.body.link).pipe(res);
